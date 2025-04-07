f40a62c5afe784ca61381d36b3d4a84205cfb577
yztwei <79294140+yztwei@users.noreply.github.com>
2025-04-07 11:57:24 +0800

    Update wzbz2.js

--- wzbz2.js
+++ wzbz2.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/bzpp3\.iwzbz\.com\/api\/.+\/(user\/getvipinfo|User\/getWXPW) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/wenzhenpaipan.js
+^https?:\/\/bzpp3\.iwzbz\.com\/api\/.+\/(user\/getvipinfo|User\/getWXPW) url script-response-body https://raw.githubusercontent.com/yztwei/js/refs/heads/main/wzbz2.js
 
 [mitm]
 hostname = bzpp3.iwzbz.com
